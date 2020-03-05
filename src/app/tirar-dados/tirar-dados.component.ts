fc242bb293c573d2c178b036cc94b10132d6b54b
cvbn <jerogassan@gmail.com>
2020-03-05 09:46:35 +0100

    Added form to random dices

--- src/app/tirar-dados/tirar-dados.component.ts
+++ src/app/tirar-dados/tirar-dados.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TirarDadosComponent implements OnInit {
 
+  tiradaResultado: number[];
+  input: number;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  dadosAleatorios() {
+    this.tiradaResultado = [];
+    for (let i = 0; i < this.input; i++) {
+      this.tiradaResultado.push(Math.floor(Math.random() * 20) + 1);
+    }
   }
 
 }
