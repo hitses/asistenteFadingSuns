5b5b88d61872767352e9c84cc475f0d0541909e2
cvbn <jerogassan@gmail.com>
2020-03-05 09:43:40 +0100

    Added froms configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 /* import { HttpClientModule } from '@angular/common/http'; */
 
 import { AppRoutingModule } from './app-routing.module';
@@ -24,6 +25,8 @@ import { CobrosComponent } from './cobros/cobros.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     /* HttpClientModule, */
   ],
   providers: [],
