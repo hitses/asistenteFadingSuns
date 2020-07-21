2a83b44f4ee7a018ec9e90063748c0f507d8803c
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2020-07-21 13:21:08 +0200

    Caracterísitcas aleatorias añadidas a personaje aleatorio

--- src/app/personaje-aleatorio/personaje-aleatorio.component.ts
+++ src/app/personaje-aleatorio/personaje-aleatorio.component.ts
@@ -20,7 +20,23 @@ export class PersonajeAleatorioComponent implements OnInit {
   adjective: string;
   powers: string[];
   power: string;
-  randomNumber: number;
+  cuerpo: number;
+  fuerza: number;
+  destreza: number;
+  resistencia: number;
+  mente: number;
+  inteligencia: number;
+  percepcion: number;
+  tecnologia: number;
+  espiritu: number;
+  extrovertido: number;
+  introvertido: number;
+  calma: number;
+  pasion: number;
+  fe: number;
+  ego: number;
+  humano: number;
+  alienigena: number;
 
   constructor() { }
 
@@ -30,14 +46,17 @@ export class PersonajeAleatorioComponent implements OnInit {
   personajeAleatorio() {
     this.generoAleatorio();
     this.razaAleatoria();
+    this.cuerpoAleatorio();
     this.claseAleatoria();
     this.característicaAleatoria();
     this.formaDeSerAleatoria();
     this.poderesAleatorios();
+    this.menteAleatorio();
+    this.espirituAleatorio();
   }
 
   generoAleatorio() {
-    this.genres = ['Mujer', 'Hombre'];
+    this.genres = ['mujer', 'hombre'];
     this.genre = this.genres[Math.floor(Math.random() * this.genres.length)];
   }
 
@@ -45,72 +64,352 @@ export class PersonajeAleatorioComponent implements OnInit {
     const FOR_HUMAN = 60;
     const FOR_OBUN = 70;
     const FOR_UKAR = 80;
-    this.randomNumber = Math.floor(Math.random() * (1 - 101)) + 101;
-    if (this.genre === 'Mujer') {
-      // tslint:disable-next-line: max-line-length
-      this.races = ['Vórox', 'Bárbara vúldrok', 'Bárbara kurgana', 'Shántor (percherona)', 'Gánnock (simiesca)', 'Etyri (mujer pájaro)', 'Hirónem (mujer lagarto)', 'Ascorbita (mujer insectoide)', 'Oro\'ym (sirena o tritona)', 'Infiltrada simbionte (no permitido salvo para partidas donde haya personajes de esta especie)', 'Mandarina Vau (no permitido salvo para partidas donde haya personajes de esta especie)'];
-      if (this.randomNumber <= FOR_HUMAN) {
-        this.race = 'Humana';
-      } else if (this.randomNumber > FOR_HUMAN && this.randomNumber <= FOR_OBUN) {
+    const RANDOM_NUMBER = Math.floor(Math.random() * (1 - 101)) + 101;
+    if (this.genre === 'mujer') {
+      this.races = [
+        'vórox',
+        'vúldrok',
+        'kurgana',
+        'shántor (percherona)',
+        'gánnock (simiesca)',
+        'etyri (mujer pájaro)',
+        'hirónem (mujer lagarto)',
+        'ascorbita (mujer insectoide)',
+        'oro\'ym (sirena o tritona)',
+        'infiltrada simbionte (no permitido salvo para partidas donde haya personajes de esta especie)',
+        'Vau (no permitido salvo para partidas donde haya personajes de esta especie)',
+      ];
+      if (RANDOM_NUMBER <= FOR_HUMAN) {
+        this.race = 'humana';
+      } else if (RANDOM_NUMBER > FOR_HUMAN && RANDOM_NUMBER <= FOR_OBUN) {
         this.race = 'Ur-Obún';
-      } else if (this.randomNumber > FOR_OBUN && this.randomNumber <= FOR_UKAR) {
+      } else if (RANDOM_NUMBER > FOR_OBUN && RANDOM_NUMBER <= FOR_UKAR) {
         this.race = 'Ur-Ukar';
-      } else if (this.randomNumber > FOR_UKAR) {
+      } else if (RANDOM_NUMBER > FOR_UKAR) {
         this.race = this.races[Math.floor(Math.random() * this.races.length)];
       }
-    } else if (this.genre === 'Hombre') {
-      // tslint:disable-next-line: max-line-length
-      this.races = ['Vórox', 'Bárbaro vúldrok', 'Bárbaro kurgano', 'Shántor (percherón)', 'Gánnock (simiesco)', 'Etyri (hombre pájaro)', 'Hirónem (hombre lagarto)', 'Ascorbita (insectoide)', 'Oro\'ym (tritón)', 'Infiltrado Simbionte (no permitido salvo para partidas donde haya personajes de esta especie)', 'Mandarín Vau (no permitido salvo para partidas donde haya personajes de esta especie)'];
-      if (this.randomNumber <= FOR_HUMAN) {
-        this.race = 'Humano';
-      } else if (this.randomNumber > FOR_HUMAN && this.randomNumber <= FOR_OBUN) {
+    } else if (this.genre === 'hombre') {
+      this.races = [
+        'vórox',
+        'vúldrok',
+        'kurgano',
+        'shántor (percherón)',
+        'gánnock (simiesco)',
+        'etyri (hombre pájaro)',
+        'hirónem (hombre lagarto)',
+        'ascorbita (insectoide)',
+        'oro\'ym (tritón)',
+        'infiltrado Simbionte (no permitido salvo para partidas donde haya personajes de esta especie)',
+        'Vau (no permitido salvo para partidas donde haya personajes de esta especie)',
+      ];
+      if (RANDOM_NUMBER <= FOR_HUMAN) {
+        this.race = 'humano';
+      } else if (RANDOM_NUMBER > FOR_HUMAN && RANDOM_NUMBER <= FOR_OBUN) {
         this.race = 'Ur-Obún';
-      } else if (this.randomNumber > FOR_OBUN && this.randomNumber <= FOR_UKAR) {
+      } else if (RANDOM_NUMBER > FOR_OBUN && RANDOM_NUMBER <= FOR_UKAR) {
         this.race = 'Ur-Ukar';
-      } else if (this.randomNumber > FOR_UKAR) {
+      } else if (RANDOM_NUMBER > FOR_UKAR) {
         this.race = this.races[Math.floor(Math.random() * this.races.length)];
       }
     }
   }
 
   claseAleatoria() {
-    if (this.genre === 'Mujer') {
-      // tslint:disable-next-line: max-line-length
-      this.classes = ['Hawkwood', 'Decados', 'Hazat', 'Li Halan', 'al-Malik', 'Ortodoxa', 'Hermana de batalla', 'Eskatónica', 'Amalteana', 'Avestita', 'Hesicasta', 'Chorali (secta de Venecia que ora con sus cantos al Pancreator)', 'Gjarti (chamanas de la naturaleza)', 'Manja (las creyentes funerarias)', 'Auriga', 'Ingeniera', 'Carroñera', 'Asambleista (Esclavista)', 'Magistrada (Cara Gris)', 'Máscara (gremio de entretenimiento de la Segunda República)', 'Feriante (La Jauría)', 'Asesina', 'Vagabunda', 'Prospectora (del espacio)', 'Apotecaria (sanadora de gremio, no eclesiástico)', 'Armera', 'Cortesana', 'Bromista', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre', 'Mujer libre'];
-      this.class = this.classes[Math.floor(Math.random() * this.classes.length)];
-    } else if (this.genre === 'Hombre') {
-      // tslint:disable-next-line: max-line-length
-      this.classes = ['Hawkwood', 'Decados', 'Hazat', 'Li Halan', 'al-Malik', 'Ortodoxo', 'Hermano de batalla', 'Eskatónico', 'Amalteano', 'Avestita', 'Hesicasto', 'Chorali (secta de Venecia que ora con sus cantos al Pancreator)', 'Gjarti (chamanes de la naturaleza)', 'Manja (los creyentes funerarios)', 'Auriga', 'Ingeniero', 'Carroñero', 'Asambleista (Esclavista)', 'Magistrado (Cara Gris)', 'Máscara (gremio de entretenimiento de la Segunda República)', 'Feriante (La Jauría)', 'Asesino', 'Vagabundo', 'Prospector (del espacio)', 'Apotecario (sanador de gremio, no eclesiástico)', 'Armero', 'Cortesano', 'Bromista', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre', 'Hombre libre'];
-      this.class = this.classes[Math.floor(Math.random() * this.classes.length)];
+    const FOR_FREE_HUMAN = 60;
+    const RANDOM_NUMBER = Math.floor(Math.random() * (1 - 101)) + 101;
+    if (this.genre === 'mujer') {
+      this.classes = [
+        'Casa Real Hawkwood',
+        'Casa Real Decados',
+        'Casa Real Hazat',
+        'Casa Real Li Halan',
+        'Casa Real al-Malik',
+        'Ortodoxa',
+        'Hermana de batalla',
+        'Eskatónica',
+        'Amalteana',
+        'Avestita',
+        'Hesicasta',
+        'Chorali (secta de Venecia que ora con sus cantos al Pancreator)',
+        'Gjarti (chamanas de la naturaleza)',
+        'Manja (las creyentes funerarias)',
+        'Auriga',
+        'Ingeniera',
+        'Carroñera',
+        'Asambleista (Esclavista)',
+        'Magistrada (Cara Gris)',
+        'Máscara (gremio de entretenimiento de la Segunda República)',
+        'Feriante (La Jauría)',
+        'Asesina', 'Vagabunda',
+        'Prospectora (del espacio)',
+        'Apotecaria (sanadora de gremio, no eclesiástico)',
+        'Armera',
+        'Cortesana',
+        'Bromista',
+      ];
+      if (RANDOM_NUMBER <= FOR_FREE_HUMAN) {
+        this.class = 'Mujer libre';
+      } else if (RANDOM_NUMBER > FOR_FREE_HUMAN) {
+        this.class = this.classes[Math.floor(Math.random() * this.classes.length)];
+      }
+    } else if (this.genre === 'hombre') {
+      this.classes = [
+        'Casa Real Hawkwood',
+        'Casa Real Decados',
+        'Casa Real Hazat',
+        'Casa Real Li Halan',
+        'Casa Real al-Malik',
+        'Ortodoxo',
+        'Hermano de batalla',
+        'Eskatónico', 'Amalteano',
+        'Avestita', 'Hesicasto',
+        'Chorali (secta de Venecia que ora con sus cantos al Pancreator)',
+        'Gjarti (chamanes de la naturaleza)',
+        'Manja (los creyentes funerarios)',
+        'Auriga',
+        'Ingeniero',
+        'Carroñero',
+        'Asambleista (Esclavista)',
+        'Magistrado (Cara Gris)',
+        'Máscara (gremio de entretenimiento de la Segunda República)',
+        'Feriante (La Jauría)',
+        'Asesino',
+        'Vagabundo',
+        'Prospector (del espacio)',
+        'Apotecario (sanador de gremio, no eclesiástico)',
+        'Armero',
+        'Cortesano',
+        'Bromista',
+      ];
+      if (RANDOM_NUMBER <= FOR_FREE_HUMAN) {
+        this.class = 'Hombre libre';
+      } else if (RANDOM_NUMBER > FOR_FREE_HUMAN) {
+        this.class = this.classes[Math.floor(Math.random() * this.classes.length)];
+      }
+    }
+  }
+
+  cuerpoAleatorio() {
+    const MIN = 3;
+    const MAX = 10;
+
+    this.cuerpo = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.fuerza = this.cuerpo;
+    this.cuerpo = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.destreza = this.cuerpo;
+    this.cuerpo = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.resistencia = this.cuerpo;
+  }
+
+  menteAleatorio() {
+    const MIN = 3;
+    const MAX = 10;
+
+    this.mente = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.inteligencia = this.mente;
+    this.mente = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.percepcion = this.mente;
+    this.mente = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.tecnologia = this.mente;
+  }
+
+  espirituAleatorio() {
+    const MIN = 3;
+    const MAX = 10;
+
+    this.espiritu = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.extrovertido = this.espiritu;
+    this.espiritu = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    if (this.espiritu + this.extrovertido <= 10) {
+      this.introvertido = this.espiritu;
+    } else {
+      this.introvertido = 10 - this.extrovertido;
+    }
+
+    this.espiritu = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.calma = this.espiritu;
+    this.espiritu = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    if (this.espiritu + this.calma <= 10) {
+      this.pasion = this.espiritu;
+    } else {
+      this.pasion = 10 - this.calma;
+    }
+
+    this.espiritu = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    this.fe = this.espiritu;
+    this.espiritu = Math.floor(Math.random() * (MIN - MAX)) + MAX;
+    if (this.espiritu + this.fe <= 10) {
+      this.ego = this.espiritu;
+    } else {
+      this.ego = 10 - this.fe;
+    }
+
+    if (this.race === 'humana' || this.race === 'humano') {
+      this.humano = 3;
+      this.alienigena = 0;
+    } else {
+      this.humano = 0;
+      this.alienigena = 3;
     }
   }
 
   característicaAleatoria() {
-    if (this.genre === 'Mujer') {
-      // tslint:disable-next-line: max-line-length
-      this.caracts = ['Calva', 'Tuerta', 'Ciega', 'Muda', 'Barbuda', 'Cicatriz en una parte de la cara', 'Manca', 'Manca total', 'Cicatriz en el pecho o brazos', 'Coja', 'Coja total', 'Ojo cibernético', 'Ojos cibernéticos', 'Altavoz cibernético', 'Oreja cibernética', 'Orejas cibernéticas', 'Brazo cibernético', 'Brazos cibernéticos', 'Pierna cibernética', 'Piernas cibernéticas', 'Elegante', 'Bella', 'Angelical', 'Desagradable', 'Fea', 'Monstruosa', 'Vista aguda', 'Oído agudo', 'Olfato agudo', 'Tacto agudo', 'Jorobada', 'Alta', 'Baja', 'Enana', 'Gigante'];
+    if (this.genre === 'mujer') {
+      this.caracts = [
+        'Calva',
+        'Tuerta',
+        'Ciega',
+        'Muda',
+        'Barbuda',
+        'Cicatriz en una parte de la cara',
+        'Manca',
+        'Manca total',
+        'Cicatriz en el pecho o brazos',
+        'Coja',
+        'Coja total',
+        'Ojo cibernético',
+        'Ojos cibernéticos',
+        'Altavoz cibernético',
+        'Oreja cibernética',
+        'Orejas cibernéticas',
+        'Brazo cibernético',
+        'Brazos cibernéticos',
+        'Pierna cibernética',
+        'Piernas cibernéticas',
+        'Elegante',
+        'Bella',
+        'Angelical',
+        'Desagradable',
+        'Fea',
+        'Monstruosa',
+        'Vista aguda',
+        'Oído agudo',
+        'Olfato agudo',
+        'Tacto agudo',
+        'Jorobada',
+        'Alta',
+        'Baja',
+        'Enana',
+        'Gigante',
+      ];
       this.caract = this.caracts[Math.floor(Math.random() * this.caracts.length)];
-    } else if (this.genre === 'Hombre') {
-      // tslint:disable-next-line: max-line-length
-      this.caracts = ['Calvo', 'Tuerto', 'Ciego', 'Mudo', 'Barbudo', 'Cicatriz en una parte de la cara', 'Manco', 'Manco total', 'Cicatriz en el pecho o brazos', 'Cojo', 'Cojo total', 'Ojo cibernético', 'Ojos cibernéticos', 'Altavoz cibernético', 'Oreja cibernética', 'Orejas cibernéticas', 'Brazo cibernético', 'Brazos cibernéticos', 'Pierna cibernética', 'Piernas cibernéticas', 'Elegante', 'Bello', 'Angelical', 'Desagradable', 'Feo', 'Monstruoso', 'Vista aguda', 'Oído agudo', 'Olfato agudo', 'Tacto agudo', 'Jorobado', 'Alto', 'Bajo', 'Enano', 'Gigante'];
+    } else if (this.genre === 'hombre') {
+      this.caracts = [
+        'Calvo',
+        'Tuerto',
+        'Ciego',
+        'Mudo',
+        'Barbudo',
+        'Cicatriz en una parte de la cara',
+        'Manco',
+        'Manco total',
+        'Cicatriz en el pecho o brazos',
+        'Cojo',
+        'Cojo total',
+        'Ojo cibernético',
+        'Ojos cibernéticos',
+        'Altavoz cibernético',
+        'Oreja cibernética',
+        'Orejas cibernéticas',
+        'Brazo cibernético',
+        'Brazos cibernéticos',
+        'Pierna cibernética',
+        'Piernas cibernéticas',
+        'Elegante',
+        'Bello',
+        'Angelical',
+        'Desagradable',
+        'Feo',
+        'Monstruoso',
+        'Vista aguda',
+        'Oído agudo',
+        'Olfato agudo',
+        'Tacto agudo',
+        'Jorobado',
+        'Alto',
+        'Bajo',
+        'Enano',
+        'Gigante',
+      ];
       this.caract = this.caracts[Math.floor(Math.random() * this.caracts.length)];
     }
   }
 
   formaDeSerAleatoria() {
-    if (this.genre === 'Mujer') {
-      // tslint:disable-next-line: max-line-length
-      this.adjectives = ['Audaz', 'Cortés', 'Curiosa', 'Disciplinada', 'Inflexible', 'Innovadora', 'Justa', 'Misericordiosa', 'Pía', 'Suspicaz', 'Altiva', 'Arrogante', 'Avara', 'Condescendiente', 'Culpable', 'Desconcertante', 'Despistada', 'Fanfarrona', 'Grosera', 'Impetuosa', 'Insensible', 'Materialista', 'Recta', 'Ruidosa', 'Sutil', 'Vanidosa', 'Vengativa', 'Casanova', 'Patosa', 'Famosa', 'Honesta'];
+    if (this.genre === 'mujer') {
+      this.adjectives = [
+        'Audaz',
+        'Cortés',
+        'Curiosa',
+        'Disciplinada',
+        'Inflexible',
+        'Innovadora',
+        'Justa',
+        'Misericordiosa',
+        'Pía',
+        'Suspicaz',
+        'Altiva',
+        'Arrogante',
+        'Avara',
+        'Condescendiente',
+        'Culpable',
+        'Desconcertante',
+        'Despistada',
+        'Fanfarrona',
+        'Grosera',
+        'Impetuosa',
+        'Insensible',
+        'Materialista',
+        'Recta',
+        'Ruidosa',
+        'Sutil',
+        'Vanidosa',
+        'Vengativa',
+        'Casanova',
+        'Patosa',
+        'Famosa',
+        'Honesta',
+      ];
       this.adjective = this.adjectives[Math.floor(Math.random() * this.adjectives.length)];
-    } else if (this.genre === 'Hombre') {
-      // tslint:disable-next-line: max-line-length
-      this.adjectives = ['Audaz', 'Cortés', 'Curioso', 'Disciplinado', 'Inflexible', 'Innovador', 'Justo', 'Misericordioso', 'Pío', 'Suspicaz', 'Altivo', 'Arrogante', 'Avaro', 'Condescendiente', 'Culpable', 'Desconcertante', 'Despistado', 'Fanfarrón', 'Grosero', 'Impetuoso', 'Insensible', 'Materialista', 'Recto', 'Ruidoso', 'Sutil', 'Vanidoso', 'Vengativo', 'Casanova', 'Patoso', 'Famoso', 'Honesto'];
+    } else if (this.genre === 'hombre') {
+      this.adjectives = [
+        'Audaz',
+        'Cortés',
+        'Curioso',
+        'Disciplinado',
+        'Inflexible',
+        'Innovador',
+        'Justo',
+        'Misericordioso',
+        'Pío',
+        'Suspicaz',
+        'Altivo',
+        'Arrogante',
+        'Avaro',
+        'Condescendiente',
+        'Culpable',
+        'Desconcertante',
+        'Despistado',
+        'Fanfarrón',
+        'Grosero',
+        'Impetuoso',
+        'Insensible',
+        'Materialista',
+        'Recto',
+        'Ruidoso',
+        'Sutil',
+        'Vanidoso',
+        'Vengativo',
+        'Casanova',
+        'Patoso',
+        'Famoso',
+        'Honesto',
+      ];
       this.adjective = this.adjectives[Math.floor(Math.random() * this.adjectives.length)];
     }
   }
 
   poderesAleatorios() {
-    // tslint:disable-next-line: max-line-length
     this.powers = ['Tiene poderes', 'No tiene poderes'];
     this.power = this.powers[Math.floor(Math.random() * this.powers.length)];
   }
